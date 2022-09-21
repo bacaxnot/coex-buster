9556c895b62e43923ddfc1e422db8afff8aa1724
Cristian Moreno <cristianfmoreno95@gmail.com>
2022-09-21 15:22:14 -0500

    se van a descargar cambios

--- src/routes/views.routes.ts
+++ src/routes/views.routes.ts
@@ -3,7 +3,9 @@ import { Router } from "express";
 const route:Router = Router();
 
 route
-    .get('/')
+    .get('/', (req, res) => {
+        res.render('components/shoppingCart.ejs')
+    })
     .get('/shop')
     .get('/shop/movie/:id')
     .get('/history')
