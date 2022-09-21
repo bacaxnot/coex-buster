98fbe7ea3763089c7004ea9e2a2e751bb1066e2a
Cristian Moreno <cristianfmoreno95@gmail.com>
2022-09-21 09:54:51 -0500

    commit antes del pull

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ class ConfigServer {
         this._app.set('PORT', config.PORT);
         this._app.set('views', path.join(__dirname, './views'));
         this._app.set('view engine', 'ejs');
-        this._app.use('/public', express.static(path.join(__dirname, '/public')));
+        this._app.use('/public', express.static(path.join(__dirname, '../public')));
         adminRoutes(this._app);
     }
 
