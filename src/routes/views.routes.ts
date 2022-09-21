12863327a8a037db742933eecb7916f22a54cf09
Mishell  Merchán <mishell2172329@correo.uis.edu.co>
2022-09-21 16:53:23 -0500

    Test routes

--- src/routes/views.routes.ts
+++ src/routes/views.routes.ts
@@ -2,8 +2,6 @@ import { Router } from "express";
 
 const route:Router = Router();
 
-const getInfo = "Después de los eventos devastadores de 'Vengadores: Infinity War', el universo está en ruinas debido a las acciones de Thanos. Con la ayuda de los aliados que quedaron, los Vengadores deberán reunirse una vez más para intentar deshacer sus acciones y restaurar el orden en el universo de una vez por todas, sin importar cuáles sean las consecuencias... Cuarta y última entrega de la saga 'Vengadores'."
-
 route
     .get('/')
     .get('/shop')
@@ -12,9 +10,5 @@ route
     .get('/history/order/:id')
     .get('/login')
     .get('/register')
-    .get('/', (req, res)=>{
-        res.render('layouts/temporal.ejs', {info:getInfo})
-
-    })
 
 export default route;
