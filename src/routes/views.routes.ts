b227ce35f49be8aaaa08d97c595e12368d16ac74
Anggie2020 <castellanos.anggie17@gmail.com>
2022-09-21 16:36:57 -0500

    main and history v1

--- src/routes/views.routes.ts
+++ src/routes/views.routes.ts
@@ -2,8 +2,16 @@ import { Router } from "express";
 
 const route:Router = Router();
 
+// route.get('/', (req, res)=>{
+//     res.render('layouts/temporal.ejs');
+// })
+
 route.get('/', (req, res)=>{
-    res.render('layouts/temporal.ejs');
+    res.render('layouts/main_movie.ejs');
+})
+
+route.get('/history', (req, res)=>{
+    res.render('layouts/history.ejs');
 })
 
 export default route;
