d61fcf79d14c3e442054c80ba32d3734d74c04df
Jean <jeancagomez94@gmail.com>
2022-09-21 14:40:30 -0500

    Creacion de componente en carpeta public

---
+++ public/scripts/carousel.js
@@ -0,0 +1,52 @@
+class Carrousel {
+  constructor() {}
+
+  render(div) {
+    return new Glider(div, {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        rewind: true,
+        draggable: true,
+        dots: ".carousel__indicadores",
+        duration: 5.0,
+        responsive: [
+          {
+            rewind: true,
+            breakpoint: 300,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 4,
+            },
+          },
+          {
+            rewind: true,
+            breakpoint: 500,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            },
+          },
+          {
+            rewind: true,
+            breakpoint: 700,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 4,
+            },
+          },
+        ],
+      });
+  }
+}
+
+
+window.onload = () => {
+
+    new Carrousel().render(document.querySelector(".carousel__lista"))
+    
+};
+
+// window.addEventListener('load', function(){
+//     // glider.refresh(true);
+
+// });
--- src/routes/views.routes.ts
+++ src/routes/views.routes.ts
@@ -2,8 +2,25 @@ import { Router } from "express";
 
 const route:Router = Router();
 
+const data = [
+    {id: "1", name: "Patrick1 Patrick1 Patrick1", img:"https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png"},
+    {id: "2", name: "Patrick2", img:"https://cdn.pixabay.com/photo/2014/03/24/17/19/teacher-295387_960_720.png"},
+    {id: "3", name: "Patrick3", img:"https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png"},
+    {id: "4", name: "Patrick4", img:"https://cdn.pixabay.com/photo/2014/03/24/17/19/teacher-295387_960_720.png"},
+    {id: "5", name: "Patrick5", img:"https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png"},
+    {id: "6", name: "Patrick6", img:"https://cdn.pixabay.com/photo/2014/03/24/17/19/teacher-295387_960_720.png"},
+    {id: "5", name: "Patrick7", img:"https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png"},
+    {id: "6", name: "Patrick8", img:"https://cdn.pixabay.com/photo/2014/03/24/17/19/teacher-295387_960_720.png"},
+    {id: "5", name: "Patrick9", img:"https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png"},
+    {id: "6", name: "Patrick10", img:"https://i.pinimg.com/originals/2d/4c/fb/2d4cfbafcb1d65a9ee3013d9a5b86417.gif"},
+    {id: "5", name: "Patrick11", img:"https://as01.epimg.net/epik/imagenes/2019/06/03/portada/1559573314_873311_1559575720_noticia_normal.jpg"},
+    {id: "6", name: "Patrick12", img:"https://ep00.epimg.net/elpais/imagenes/2016/05/24/album/1464082468_050087_1464083762_album_normal.jpg"},
+    {id: "6", name: "Patrick13", img:"https://images.unsplash.com/photo-1580518337843-f959e992563b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YWN0b3Jlc3xlbnwwfHwwfHw%3D&w=1000&q=80"},
+]
+
+ 
 route.get('/', (req, res)=>{
-    res.render('layouts/temporal.ejs');
+    res.render('layouts/temporal.ejs', {people:data});
 })
 
 export default route;
