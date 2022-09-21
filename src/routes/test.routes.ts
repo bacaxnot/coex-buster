25e9f5c4d18494c396bce66726d3f00b2397b069
Anggie2020 <castellanos.anggie17@gmail.com>
2022-09-21 17:24:35 -0500

    avance en orders

--- src/routes/test.routes.ts
+++ src/routes/test.routes.ts
@@ -6,7 +6,7 @@ router.get('/shop', (req, res)=>{
     res.render('layouts/main_movie.ejs');
 })
 
-router.get('/history/order:id', (req, res)=>{
+router.get('/history/order', (req, res)=>{  //colocarle al order:id
     res.render('layouts/history.ejs');
 })
 
