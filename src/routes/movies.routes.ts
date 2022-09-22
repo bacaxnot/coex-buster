a4ce175d232f79df030856df4e49eeaf224190c9
Jerson Guerrero <yesidlds54@gmail.com>
2022-09-22 16:30:41 -0500

    added routes for movies

--- src/routes/movies.routes.ts
+++ src/routes/movies.routes.ts
@@ -2,20 +2,29 @@ import { Router, Request, Response } from "express";
 
 const router : Router = Router();
 
-router.get('/movies', (req, res) => {
-    const data = {
-        "movies": [
-            {
-                "id": 1,
-                "title": "Scary Movie"
-            },
-            {
-                "id": 2,
-                "title": "Saw"
-            }
-        ]
+const movies = [
+    {
+        "id": "1",
+        "title": "Scary Movie"
+    },
+    {
+        "id": "2",
+        "title": "Saw"
     }
-    res.send(data)
+]
+
+router.get('/', (req, res) => {
+    res.redirect('movies')
+});
+
+router.get('/movies', (req, res) => {
+    res.send(movies)
+});
+
+router.get('/movies/:id', (req, res) => {
+    const {id} = req.params;
+    const result = movies.find(movie => movie.id == id);
+    res.send(result) 
 });
 
 export default router
