848500fd1c766e5549c338eafdc3e333c0aabebe
Cristian Moreno <cristianfmoreno95@gmail.com>
2022-09-22 11:04:53 -0500

    database añadida con datos de DB temporal

---
+++ src/helpers/optionConnection.ts
@@ -0,0 +1,28 @@
+import {Op, Sequelize, Options} from "sequelize";
+
+
+
+class OptionsConnection {
+    private _sequelize:Sequelize
+
+    constructor(options: Options){
+        this._sequelize = new Sequelize(options)
+    }
+
+    get sequelize(): Sequelize {
+        return this._sequelize
+    }
+
+    async authenticate():Promise<void>{
+        try {
+            await this._sequelize.authenticate();
+            await this._sequelize.sync({alter: true});
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+
+export default OptionsConnection;
+
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,13 +1,5 @@
 import { Router } from "express";
 
-<<<<<<< HEAD
-import { Router } from 'express';
-
-const router:Router = Router();
-
-export default router;
-=======
 const route: Router = Router()
 
 export default route
->>>>>>> shoppingCart
