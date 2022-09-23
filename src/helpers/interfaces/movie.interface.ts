249984c8c6b39e3f948cca4bdc21e1c0e95eea45
Cristian Moreno <cristianfmoreno95@gmail.com>
2022-09-23 12:37:10 -0500

    carpetas de interfaces innecesarias eliminadas debido al uso de prisma, cambio de interfaces de acuerdo a los requerimienttos del ORM

--- src/helpers/interfaces/movie.interface.ts
+++ src/helpers/interfaces/movie.interface.ts
@@ -1,23 +1,9 @@
-import { IGetAll, IGetOne } from "./crud.interface";
-
-export interface IMovieModel {
-    id:number,
-    title:string,
-    overview:string,
-    posterPath:string,
-    relaseDate:Date,
-    popularity:number,
-    voteAverage:number,
-    voteCount:number,
-    isForAdults:boolean,
-    language:string,
-    runtime: number,
-    videoKey:string,
-}
+import { IGetAll, IGetOne, IController } from "./crud.interface";
+import { Request, Response } from "express";
 
 export interface IMovieRepository<TModel> extends IGetAll<TModel>, IGetOne<TModel, number> {}
 
-export interface IMovieController<TModel> extends IGetAll<TModel>, IGetOne<TModel, number> {}
+export interface IMovieController<TModel> extends IController<Request, Response> {}
 
 
 
--- src/helpers/interfaces/transaction.interface.ts
+++ src/helpers/interfaces/transaction.interface.ts
@@ -1,6 +1,7 @@
-import { ICreate, IGetAll, IGetOne } from "./crud.interface";
+import { Request, Response } from "express";
+import { ICreate, IGetAll, IGetOne, IController } from "./crud.interface";
 
 
-export interface ITransactionController<TModel> extends IGetAll<TModel>, IGetOne<TModel, number>, ICreate<TModel> {}
+export interface ITransactionController<TModel> extends IController<Request, Response> {}
 
 export interface ITransactionRepository<TModel> extends IGetAll<TModel>, IGetOne<TModel, number>, ICreate<TModel> {}
