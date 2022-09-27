96a10452f122f38ea14697229ab21726e257a673
tomasduh <tomasduh421@gmail.com>
2022-09-27 17:47:57 -0500

    modificacion de comparacion de psd y bd

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express"
 import { IController } from "../helpers/interfaces/crud.interface"
+import '../helpers/middlewares/camparePassword'
+import usersRepository from "../repository/users.repository"
+import bcrypt from "bcrypt";
 
 
 class LoginController implements IController<Request, Response> {
@@ -7,7 +10,14 @@ class LoginController implements IController<Request, Response> {
     
     async signIn(req: Request, res: Response): Promise<void> {
         const {email, password} = req.body
-        res.json({email, password})
+        const user = await usersRepository.getEmail(email)
+        const hashPassword = user.password
+        const result = bcrypt.compare(password, hashPassword);
+        if(!result){
+            throw new Error('contraseña incorrecta')
+        }
+        // res.render('/shop', user.email)
+        res.json({result})
     }
 
     async signUp(req: Request, res: Response): Promise<void> {
