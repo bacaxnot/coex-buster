cbb411d5f13713d7e4786c38fe5612114c007770
Jerson Guerrero <yesidlds54@gmail.com>
2022-09-29 12:35:22 -0500

    corrected layout errors and implementation of new functionalities

--- src/routes/views.routes.ts
+++ src/routes/views.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import viewController from "../controllers/ViewController";
 import tokenAuthentication from "../helpers/middlewares/tokenAuthentication";
+import transactionController from "../controllers/transaction.controller";
+import transaction_detailController from "../controllers/transaction_detail.controller";
 
 const router: Router = Router();
 
@@ -11,8 +13,9 @@ router
 
     .get('/movies/:pag', viewController.getAll)
     .get('/movie/:id')
-    .get('/history', tokenAuthentication)
-    .get('/history/order/:id', tokenAuthentication)
+    // .get('/history', tokenAuthentication)
+    .get('/history', transactionController.getAll)
+    .get('/history/order/:id', transaction_detailController.getOne)
     .get('/login', (req, res) => {
         res.render('layouts/login')
     })
